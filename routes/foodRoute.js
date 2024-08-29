188e02d42da3f633c14a758c19f5ca712e438c94
Robeeen <robeeen@gmail.com>
2024-08-29 19:49:48 +0600

    storage system

--- routes/foodRoute.js
+++ routes/foodRoute.js
@@ -6,7 +6,18 @@ const foodRouter = express.Router();
 
 
 
+//image storing system
 
+const storage = multer.diskStorage({
+    destination: "uploads",
+    filename:(req,file,cb)=> {
+        return cb(null, `${Date.now()}${file.originalname}`)
+    }
+})
+
+const upload = multer({storage: storage})
+
+foodRouter.post("/add", addFood)
 
 
 
